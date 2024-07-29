2bbf72871c0db9ba4e1cd67f53d36e7135beb6d8
subramanyamposhala <154345280+subramanyamposhala@users.noreply.github.com>
2024-07-29 11:54:06 +0530

    Add files via upload

---
+++ LEVEL 1/Task 3/Temperature converter website/script.js
@@ -0,0 +1,15 @@
+document.getElementById('converter').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const degrees = parseFloat(document.getElementById('degrees').value);
+    const type = document.getElementById('type').value;
+    let result;
+
+    if (type === 'Fahrenheit') {
+        result = (degrees - 32) * 5 / 9;
+    } else {
+        result = (degrees * 9 / 5) + 32;
+    }
+
+    document.getElementById('result').value = result.toFixed(4);
+});
